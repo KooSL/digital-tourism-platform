9f7be52ee99b476dce4b475ead0c0d5322c4a85a
KooSL <comp.kushal@gmail.com>
2026-05-24 17:45:30 +0545

    Refactor payment calculation algorithm to include dynamic subtotal and discounts and tax handling

--- esewa-payment.php
+++ esewa-payment.php
@@ -15,8 +15,24 @@ if (!isset($_SESSION['booking_data'])) {
 $pid = "BOOK_" . time();
 $_SESSION['pid'] = $pid;
 
-$secret_key = "8gBm/:&EnhH.1/q";
-$total_amount = $data['amount'] + 10;
+$secret_key = "8gBm/:&EnhH.1/q"; 
+
+$subtotal = $data['amount'] * $data['persons'];
+
+if ($data['persons'] >= 5 && $data['persons'] < 10) {
+    $subtotal *= 0.9; // 10% discount
+}
+
+if ($data['persons'] >= 10 && $data['persons'] <= 15) {
+    $subtotal *= 0.8; // 20% discount
+}
+
+$tax_amount = 10;
+
+$total_amount = $subtotal + $tax_amount;
+
+$total_amount = round($total_amount, 2);
+
 $product_code = "EPAYTEST";
 
 $payload = "total_amount=$total_amount,transaction_uuid=$pid,product_code=$product_code";
@@ -27,8 +43,8 @@ $signature = base64_encode(hash_hmac('sha256', $payload, $secret_key, true));
 
 <body>
     <form action="https://rc-epay.esewa.com.np/api/epay/main/v2/form" method="POST">
-        <input type="text" id="amount" name="amount" value="<?php echo $data['amount']; ?>" required>
-        <input type="text" id="tax_amount" name="tax_amount" value="10" required>
+        <input type="text" id="amount" name="amount" value="<?php echo $subtotal; ?>" required>
+        <input type="text" id="tax_amount" name="tax_amount" value="<?php echo $tax_amount; ?>" required>
         <input type="text" id="total_amount" name="total_amount" value="<?php echo $total_amount; ?>" required>
         <input type="text" id="transaction_uuid" name="transaction_uuid" value="<?php echo $pid; ?>" required>
         <input type="text" id="product_code" name="product_code" value="<?php echo $product_code; ?>" required>
